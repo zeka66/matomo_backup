4e05f99783fda07d4f27b0830be04d748d709097
zeka66 <zeka66@gmail.com>
2022-04-27 11:33:33 +0300

    with delete old files adding

--- matomo_backup.sh
+++ matomo_backup.sh
@@ -8,6 +8,7 @@ db_pass='abc'
 db_name='matomo'
 db_port='3306'
 date=$(date '+%d.%m.%Y_%T')
+rt='0'
 
 ### Matomo backup and archive ###
 
@@ -15,13 +16,17 @@ mkdir -p ${db_backup_path}
 echo "Backup started for database - ${db_name}"
 
 sudo mysqldump\
-	-P${db_port}\
-      	-u${db_user}\
-       	-p${db_pass}\
-      	--no-tablespaces ${db_name} | gzip -c >${db_backup_path}/${date}.gz
+        -P${db_port}\
+        -u${db_user}\
+        -p${db_pass}\
+        --no-tablespaces ${db_name} | gzip -c >${db_backup_path}/${date}.gz
 
-echo "${db_name} DB backup completed"
+echo "$db_name DB backup completed"
 
 ### Copy backup to S3 bucket ###
 
-aws s3 sync //backup/matomodb/ s3://devops-backup-storage/matomo_backup/ --delete --profile holmtestprofile
+#aws s3 sync //backup/matomodb/ s3://devops-backup-storage/matomo_backup/ --delete --profile holmtestprofile
+
+
+# Remove backups more than 30 days old
+find $db_backup_path -name "*.gz" -type f -mtime +$rt -delete
